5f4f9161c1695e258249e96f298eaab5ba163547
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-20 10:56:32 +0300

    complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+    def match (array_of_words)
+        array_of_words.filter do |letter|
+            sorted_letters = letter.split('').sort 
+            word_elements = word.split('').sort
+            sorted_letters == word_elements
+        end
+
+
+    end
+
+end
